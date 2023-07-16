ee2bbfbb01aa01b2261e331a266459253ec830bb
Burmikk <byrmik@gmail.com>
2023-07-16 14:00:03 +0300

    add footer info

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -12,13 +12,27 @@ const Footer = () => {
             <div className={scss.footer_box}>
                 <div className={scss.info_wrapper}>
                     <div className={scss.contacts}>
-                        <a href="tel:+380676322719">+380 67 632 27 19</a>
-                        <a href="mailto:sales@fortnox.com">sales@fortnox.com</a>
+                        <a className={scss.link} href="tel:+380990929510" aria-label="Instagram">
+                            +38 099 092 95 10
+                        </a>
+                        <a className={scss.link} href="tel:+380676322719">
+                            +38 067 632 27 19
+                        </a>
+                        <a className={scss.link} href="mailto:fortnox.office48@gmail.com">
+                            fortnox.office48@gmail.com
+                        </a>
                     </div>
-                    <div className={scss.contacts}>
-                        <p>вул. Перемоги, 48, Запоріжжя, </p>
-                        <p>Запорізька обл., 69000</p>
-                    </div>
+                    <a
+                        className={scss.link}
+                        href="https://goo.gl/maps/YqtcnE4NqJ8cceXh8"
+                        target="_blank"
+                        rel="noreferrer noopener"
+                    >
+                        <div className={scss.contacts}>
+                            <p>вул. Перемоги, 48, Запоріжжя, </p>
+                            <p>Запорізька обл., 69000</p>
+                        </div>
+                    </a>
                 </div>
                 <div className={scss.copyright}>
                     <p>&copy; 2023 Форт Нокс. Всі права захищені.</p>
