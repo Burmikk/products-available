ed44f983471c684a076b55856b0dadc1ca9cbcc4
Burmikk <byrmik@gmail.com>
2023-07-31 00:58:54 +0300

    delete layout

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import NavBar from "./components/NavBar/NavBar";
 import MainRouter from "MainRouter";
 import Footer from "components/Footer/Footer";
-import Layout from "components/Layout/Layout";
 
 const App = () => {
     return (
-        // <Layout>
         <>
             <NavBar />
             <MainRouter />
             <Footer />
         </>
-        // </Layout>
     );
 };
 
